5f826335be8164443108c8907737349b5789f179
Alla Karhopolova <alalasimys@gmail.com>
2021-07-23 23:35:51 +0300

    add src/array-practice/task1.js

--- src/array-practice/task1.js
+++ src/array-practice/task1.js
@@ -15,13 +15,9 @@
  * console.log(any([0, 0, 0, 0])); -> false
  */
 
-function any(arr, callback) {
+export default function any(arr, callback) {
   if (callback) {
     return arr.some(elem => callback(elem));
   }
   return arr.some(elem => Boolean(elem));
 }
-
-console.log(any([0, 1, 2, 0], x => x >= 2)); // -> true
-console.log(any([0, 0, 1, 0])); // -> true
-console.log(any([0, 0, 0, 0])); // -> false
