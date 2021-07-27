a1179f2202954ffc1417e570771c1715507678a3
Alla Karhopolova <alalasimys@gmail.com>
2021-07-27 19:53:01 +0300

    add asynchronous-programming/task_2.js

---
+++ src/asynchronous-programming/task_2.js
@@ -0,0 +1,19 @@
+function parseJSON(jsonStr, success, failure) {
+  try {
+    const result = JSON.parse(jsonStr);
+    return success(result);
+  } catch (error) {
+    return failure(error);
+  }
+}
+
+function successCb(result) {
+  return console.log('Seccess parse!', result);
+}
+
+function failureCb(error) {
+  return console.log('Error parse!', error);
+}
+
+parseJSON('{ "x": 10 }', successCb, failureCb);
+parseJSON('{ x }', successCb, failureCb);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './styles/main.css';
 
-import './asynchronous-programming/task_1';
+// import './asynchronous-programming/task_1';
+import './asynchronous-programming/task_2';
