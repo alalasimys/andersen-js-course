360d5d617db4dad995c52ea5db5b622a538b8668
Alla Karhopolova <alalasimys@gmail.com>
2021-07-28 13:32:36 +0300

    asynchronous-programming/task_5.js

---
+++ src/asynchronous-programming/task_6.js
@@ -0,0 +1,7 @@
+// function getResolvedPromise(value) {
+//   return Promise.resolve(value).then(value => {
+//     if (value > 300) {
+//       throw new Error('Ошибка!');
+//     }
+//   });
+// }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import './styles/main.css';
 // import './asynchronous-programming/task_1';
 // import './asynchronous-programming/task_2';
 // import './asynchronous-programming/task_3';
-import './asynchronous-programming/task_4';
+// import './asynchronous-programming/task_4';
+import './asynchronous-programming/task_5';
+// import './asynchronous-programming/task_6';
